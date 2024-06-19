efe5c1cbfce1da07f68256f84a1188bb7e0416f9
JacLealMena <jac.leal@duocuc.cl>
2024-06-19 08:55:01 -0400

    Inicio del ejercicio

---
+++ funciones.py
@@ -0,0 +1,19 @@
+#Opcion 1
+
+def opcion_1():
+    pass
+
+#Opcion 2
+
+def opcion_2():
+    pass
+
+#Opcion 3
+
+def opcion_3():
+    pass
+
+#opcion  4
+
+def opcion_4():
+    pass
---
+++ principal.py
@@ -0,0 +1,18 @@
+from funciones import *
+
+while True:
+    print("""
+\tMenú Contactos
+1. Agregar contacto
+2. Mostrar contactos
+3. Guardar Archivo
+4. Salir""")
+    opc = int(input("Opción: "))
+    if opc == 1:
+        pass
+    if opc == 2:
+        pass
+    if opc == 3:
+        pass
+    else:
+        pass
